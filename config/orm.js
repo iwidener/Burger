66fae38badf15d8fd84d503ad118729a41b43e66
Inna Widener <iwide2019@gmail.com>
2020-03-22 16:27:50 -0500

    Config Setup completed

---
+++ config/orm.js
@@ -0,0 +1,66 @@
+const connection = require("./connection.js");
+
+function printQmarks(num) {
+
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
+function objToSql(ob) {
+
+    var arr = [];
+
+    for (var key in ob) {
+        var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+        return arr.toString();
+}
+
+const orm = {
+
+    selectAll: function (tableInput, cb) {
+        const queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+    insertOne: function (tableInput, cols, vals, cb) {
+        const queryString = "INSERT INTO " + tableInput + " (" + cols.toString() + ") " + "VALUES (" + printQmarks(vals.length) + ") ";
+
+        console.log(queryString);
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+    updateOne: function (tableInput, objColVals, condition, cb) {
+        const queryString = "UPDATE " + tableInput + " SET " + objToSql(objColVals) + " WHERE " + condition;
+
+        console.log(queryString);
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+};
+
+module.exports = orm;
