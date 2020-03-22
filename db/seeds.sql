ab7d91e00591e66b41c400a6e3ce82c3ee116e92
Inna Widener <iwide2019@gmail.com>
2020-03-21 22:54:41 -0500

    App Setup completed. DB Setup completed.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Classical Angus Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grilled Bison Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Chicken Burger', true);
