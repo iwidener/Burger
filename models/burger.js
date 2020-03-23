74ea0f4be5ae12e60cea3a3217b687bc67dffb2d
Inna Widener <iwide2019@gmail.com>
2020-03-22 21:25:03 -0500

    Model Setup completed

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+const orm = require("../config/orm.js");
+
+const burger = {
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
+
+    insertOne: function (cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
