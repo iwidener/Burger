95e51f901aa27ef9faebc5dc1aa039c6e3240301
Inna Widener <iwide2019@gmail.com>
2020-03-22 22:45:12 -0500

    Server file completed.

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Listening at localhost:" + PORT);
+});
