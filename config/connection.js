27527dc9dde96ed66c97c99b528741bb4cbf1952
Inna Widener <iwide2019@gmail.com>
2020-03-24 23:12:22 -0500

    npm dotenv installed

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 const connection = mysql.createConnection({
-    host: "localhost",
+    host: process.env.DB_HOST,
     port: 3306,
-    user: "root",
-    password: "password",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: "burgers_db"
 });
 
