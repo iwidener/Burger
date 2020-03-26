7f9b854c610a890f768b0cbd9eaebff1e5d725be
Inna Widener <iwide2019@gmail.com>
2020-03-25 22:47:44 -0500

    Changed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,17 @@
 const mysql = require("mysql");
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: "burgers_db"
-});
+const connection;
 
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: "burgers_db"
+    });
+}
 
+connection.connect();
 module.exports = connection;
